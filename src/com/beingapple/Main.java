0cdf57314d33cee68bee06c0cdd25d99d5c222e0
Yongbi Jo <whdydql123@naver.com>
2019-07-24 20:16:00 +0900

    사용자 입력 추가
    Scanner를 통한 사용자 입력 추가

--- src/com/beingapple/Main.java
+++ src/com/beingapple/Main.java
@@ -1,11 +1,29 @@
 package com.beingapple;
 
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-	    LookAndSay lookAndSay = new LookAndSay(1, 10);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("시작할 숫자를 입력하세요(0 < R < 100) : ");
+        int startNumber = Integer.parseInt(scan.nextLine());
+
+        while(startNumber >= 100 || startNumber <= 0){
+            System.out.print("범위 내의 숫자를 입력하세요 : ");
+            startNumber = Integer.parseInt(scan.nextLine());
+        }
+
+        System.out.print("몇 번째 줄의 수열을 확인하시겠습니까? : ");
+        int lineNumber = Integer.parseInt(scan.nextLine());
+
+        while(lineNumber > 25 || lineNumber <= 0){
+            System.out.print("범위 내의 숫자를 입력하세요 : ");
+            lineNumber = Integer.parseInt(scan.nextLine());
+        }
+
+	    LookAndSay lookAndSay = new LookAndSay(startNumber, lineNumber);
         Iterator<Integer> result = lookAndSay.makeLookAndSay();
 
         while(result.hasNext()){
